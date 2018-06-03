7cc2237a49b4b5e293319141ab1a2492127c806c
flar2 <asegaert@gmail.com>
2018-06-03 12:29:02 -0400

    leds-qpnp-haptics: allow user adjustment
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/leds/leds-qpnp-haptics.c
+++ drivers/leds/leds-qpnp-haptics.c
@@ -357,6 +357,7 @@ struct hap_chip {
 	bool				play_irq_en;
 	bool				auto_res_err_recovery_hw;
 	bool				vcc_pon_enabled;
+	int				vmax_override;
 };
 
 struct hap_chip *gchip;
@@ -1514,7 +1515,7 @@ static ssize_t qpnp_haptics_show_duration(struct device *dev,
 
 	if (hrtimer_active(&chip->stop_timer)) {
 		time_rem = hrtimer_get_remaining(&chip->stop_timer);
-		time_us = ktime_to_us(time_rem);
+W		time_us = ktime_to_us(time_rem);
 	}
 
 	return snprintf(buf, PAGE_SIZE, "%lld\n", div_s64(time_us, 1000));
@@ -1814,6 +1815,9 @@ static ssize_t qpnp_haptics_store_vmax(struct device *dev,
 	if (rc < 0)
 		return rc;
 
+	if (chip->vmax_override)
+		return count;
+
 	old_vmax_mv = chip->vmax_mv;
 	chip->vmax_mv = data;
 	rc = qpnp_haptics_vmax_config(chip, chip->vmax_mv, false);
@@ -1822,6 +1826,91 @@ static ssize_t qpnp_haptics_store_vmax(struct device *dev,
 		return rc;
 	}
 
+	return count;
+}
+
+static ssize_t qpnp_haptics_show_vmax_mv_user(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct led_classdev *cdev = dev_get_drvdata(dev);
+	struct hap_chip *chip = container_of(cdev, struct hap_chip, cdev);
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", chip->vmax_mv);
+}
+
+static ssize_t qpnp_haptics_store_vmax_mv_user(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct led_classdev *cdev = dev_get_drvdata(dev);
+	struct hap_chip *chip = container_of(cdev, struct hap_chip, cdev);
+	int data, rc, old_vmax_mv;
+
+	rc = kstrtoint(buf, 10, &data);
+	if (rc < 0)
+		return rc;
+
+	old_vmax_mv = chip->vmax_mv;
+	chip->vmax_mv = data;
+	rc = qpnp_haptics_vmax_config(chip, chip->vmax_mv, false);
+	if (rc < 0) {
+		chip->vmax_mv = old_vmax_mv;
+		return rc;
+	}
+
+	return count;
+}
+
+static ssize_t qpnp_haptics_show_vmax_override(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct led_classdev *cdev = dev_get_drvdata(dev);
+	struct hap_chip *chip = container_of(cdev, struct hap_chip, cdev);
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", chip->vmax_override);
+}
+
+static ssize_t qpnp_haptics_store_vmax_override(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct led_classdev *cdev = dev_get_drvdata(dev);
+	struct hap_chip *chip = container_of(cdev, struct hap_chip, cdev);
+	int data, rc;
+
+	rc = kstrtoint(buf, 10, &data);
+	if (rc < 0)
+		return rc;
+
+	if (data != 0 && data != 1)
+		return count;
+
+	chip->vmax_override = !!data;
+
+	return count;
+}
+
+static ssize_t qpnp_haptics_show_rf_hz(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	u8 lra_auto_res_lo = 0, lra_auto_res_hi = 0;
+	u32 temp;
+	struct led_classdev *cdev = dev_get_drvdata(dev);
+	struct hap_chip *chip = container_of(cdev, struct hap_chip, cdev);
+
+	qpnp_haptics_read_reg(chip, HAP_RATE_CFG1_REG(chip),
+				&lra_auto_res_lo, 1);
+	qpnp_haptics_read_reg(chip, HAP_RATE_CFG2_REG(chip),
+				&lra_auto_res_hi, 1);
+
+	temp = (lra_auto_res_hi  << 8) | (lra_auto_res_lo & 0xFF);
+
+	chip->resonant_frequency = ((19200/96)*1000)/temp;
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", chip->resonant_frequency);
+}
+
+static ssize_t qpnp_haptics_store_rf_hz(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
 	return count;
 }
 
@@ -1867,6 +1956,8 @@ static struct device_attribute qpnp_haptics_attrs[] = {
 	__ATTR(wf_s_rep_count, 0664, qpnp_haptics_show_wf_s_rep_count,
 		qpnp_haptics_store_wf_s_rep_count),
 	__ATTR(vmax_mv, 0664, qpnp_haptics_show_vmax, qpnp_haptics_store_vmax),
+	__ATTR(vmax_override, 0664, qpnp_haptics_show_vmax_override, qpnp_haptics_store_vmax_override),
+	__ATTR(vmax_mv_user, 0664, qpnp_haptics_show_vmax_mv_user, qpnp_haptics_store_vmax_mv_user),
 	__ATTR(lra_auto_mode, 0664, qpnp_haptics_show_lra_auto_mode,
 		qpnp_haptics_store_lra_auto_mode),
 };
